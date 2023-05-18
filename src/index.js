9b3ea758c35a744e2efde190bc851f4796cc6fae
Canh Truong <canhtruong2412@gmail.com>
2023-05-18 12:31:36 +0700

    hello

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const { json } = require('express');
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+//get css direction
+app.use(express.static(__dirname + '/public'));
+
+//them express handlebars
+
+const { engine } = require ('express-handlebars');
+
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set("views", "./src/resources/views");
+
+
+// them thu vien morgan de quan ly log
+/*
+const morgan = require('morgan')
+app.use(morgan('combined'))*/
+
+// neu then trangchu vao sau / thi luc duyet web cung phai them trang chu sau port
+app.get('/', (req, res) => {
+  res.render('home')
+})
+// tao link toi trang newtab
+app.get('/newtab', (req, res) => {
+  res.render('newtab')
+
+})
+// tao link toi trang newtab
+app.get('/search', (req, res) => {
+  res.render('search')
+  // alert content of query
+  console.log(req.query)
+})
+//post method
+app.use(express.urlencoded({
+  extended:true
+}))
+app.use(json())
+app.post('/search', (req, res) => {
+  res.send('search')
+  console.log(req.body)
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
